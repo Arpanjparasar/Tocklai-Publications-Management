89847a8ddea124d5cb5191414439d97e845c7a74
Arpanjparasar <arpangeet@gmail.com>
2018-07-09 19:32:06 +0530

    ebook form updated

--- admin/addebook.php
+++ admin/addebook.php
@@ -25,7 +25,7 @@
 		<div class="a1-container a1-dark-gray a1-center">
         	<h6>ADD NEW EBOOK</h6>
         </div>
-       <form id="form1" name="form1" method="post" class="a1-container" action="savecd.php">
+       <form id="form1" name="form1" method="post" class="a1-container" action="saveebook.php">
          <table width="100%" border="0" align="center">
          <tr>
            <td height="35"><table width="100%" border="0" align="center">
