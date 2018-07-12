9240d046aa0c686085d0d2e90ecfcc64530040da
Rocktim <rocktim53@gmail.com>
2018-07-13 02:12:03 +0530

    checkout.php and checkout_db.php file added

---
+++ checkout.php
@@ -0,0 +1,62 @@
+<?php
+ session_start();
+ extract($_POST);
+ extract($_SESSION);
+ 
+require('connect.php'); 	
+	//echo $uid;
+	$total=$_POST['tot'];
+
+?>
+
+
+<html>
+<head>
+<meta charset="utf-8">
+<title>TRA</title>
+<link href="a1style.css" rel="stylesheet" type="text/css">
+<link href="style.css" rel="stylesheet" type="text/css">
+
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<link rel="stylesheet" href="index_files/mbcsmbmcp.css" type="text/css" />
+
+</head>
+
+<body>
+<?php include('menu.php'); ?>
+ <div id="banner">
+		<img src="images/banner.jpg" width="100%" height="170"  alt=""/> 
+
+</div><br><br>
+
+            <div class="freshdesignweb-top">
+                <div class="clr"></div>
+				
+            </div>
+		
+      <div  class="form">
+    		<form id="contactform" method="post" action="checkout_db.php"> 
+    			<p class="contact"><label for="name">Name</label></p> 
+    			<input id="name" name="name" placeholder="First and last name" required="" tabindex="1" type="text"> 
+				 <p class="contact"><label for="name">Category</label></p> 
+    			 <input id="category" name="category" required="" tabindex="1" type="radio" value="Member"> Member  &nbsp;&nbsp;&nbsp;
+				 <input id="category" name="category" required="" tabindex="1" type="radio" value="Non-Member"> Non Member  
+    			 
+    			<p class="contact"><label for="email">Address</label></p> 
+    			<textarea id="address" name="address" placeholder="Address" required="" cols="45" row="10"type="email"> </textarea>
+                
+
+            <p class="contact"><label for="phone">Mobile phone</label></p> 
+            <input id="phone" name="phone" required="" type="text"> <br>
+			<p class="contact"><label for="name">Total</label></p> 
+    			<input id="total" name="total" value="<?php echo $total; ?>" readonly tabindex="1" type="text"> 
+
+
+
+				
+            <input class="buttom" name="submit" id="submit" tabindex="5" value="Confirm & Proceed" type="submit"> 	 
+   </form> 
+</div>      
+</div>
+</body>
+</html>
---
+++ checkout_db.php
@@ -0,0 +1,65 @@
+<?php
+ session_start();
+//  $user=$_SESSION['unm'];
+$user="hello";
+require('connect.php');
+			$name=$_POST['name'];
+			$address=$_POST['address'];
+
+			$phone=$_POST['phone'];
+			$total=$_POST['total'];
+
+            $category=$_POST['category'];
+            
+            date_default_timezone_set('Asia/Kolkata');
+            $odate=date("Y-m-d h:i:s");
+
+$query5="insert into orders(sellername,name,category,address,mobile,total,orderdate) values('$user','$name','$category','$address','$phone','$total','$odate')";
+    mysqli_query($conn,$query5) or die("can't Execute... query5");
+
+    $query2="select * from cart where user='".$user."'";
+    $res2=mysqli_query($conn,$query2) or die("can't Execute... res2");
+
+    $query8="select MAX(orderid) AS max from orders";
+    $res8=mysqli_query($conn,$query8) or die("can't Execute... res8");
+
+    $row8=mysqli_fetch_array($res8);
+
+    while($row=mysqli_fetch_assoc($res2)) {
+        $query = "insert into itemsold(id,itemid,user,name,type,quantity,rate,discount,time) values('" . $row8['max'] . "','" . $row['itemid'] . "','" . $row['user'] . "','" . $row['name'] . "','" . $row['type'] . "','" . $row['quantity'] . "','" . $row['rate'] . "','" . $row['discount'] . "','$odate')";
+
+        if ($row['type'] != "ebook") {
+            if($row['type']=='cd'){
+                $idd="cid";
+            }
+            else{
+                $idd="bid";
+            }
+                $query1 = "select copies from ".$row['type']." where ".$idd."='" . $row['itemid'] . "'";
+    
+            $res = mysqli_query($conn, $query1) or die("could not Execute...");
+            $row2=mysqli_fetch_assoc($res);
+
+            $n=$row2['copies']-$row['quantity'];
+            $query4="update " . $row['type'] . " set copies=".$n." where ".$idd."=".$row['itemid']."";
+           mysqli_query($conn,$query4) or die("cannot Execute...");
+        }
+        mysqli_query($conn,$query) or die("can't Execute...");
+    }
+$query6="delete from cart where user='".$user."'";
+
+if(mysqli_query($conn,$query6)){
+    $message = "Order placed successfully.☺";
+    echo "<script type='text/javascript'>alert('$message');
+    window.location.href='userhome.php';</script>";
+} 
+else{
+    $message = "Order placed failed!.";
+    echo "<script type='text/javascript'>alert('$message');
+    window.location.href='userhome.php';</script>";
+}
+
+
+
+
+?>
